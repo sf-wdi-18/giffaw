b63d73c967d7d1a188b83cf8e05ceb49a23a727e
rycwilson <rycwilson@gmail.com>
2015-05-12 10:56:42 -0700

    solution with bootstrap

--- base.js
+++ base.js
@@ -1,4 +1,28 @@
-$(document).ready(function(){
-  // code in here!
-  console.log("here's your data:", mockData)
-})
+$(document).ready(function() {
+
+  var giphyAPI = 'http://api.giphy.com/v1/gifs/search?api_key=dc6zaTOxFJmzC';  // q=funny+cat&
+  var giphyAPItrending = 'http://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC';
+  var search = '';
+
+  // load the local data
+  render(mockData);
+
+  // load trending gifs
+  // $.get(giphyAPItrending, render);
+
+  // search the Giphy API
+  $('form').on('submit', function(event) {
+    event.preventDefault();
+    search = $(this.api_search).val();
+    $.get(giphyAPI + '&q=' + search, render);   // $.getJSON also works
+  });
+
+  function render(data) {
+    // clear the previous results
+    $('div.row').empty();
+    data.data.forEach(function(gif) {
+        $('div.row').append("<img src='" + gif.images.fixed_height.url +  "'>");
+    });
+  }
+
+});
